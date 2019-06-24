042f21b7325382760ba76111f04fa7bb61d3ec53
atieh <atiehbaratinia@gmail.com>
2019-06-24 21:24:00 +0430

    create new Activity, Addition

--- app/src/main/java/com/example/effectAndEdit/Addition.java
+++ app/src/main/java/com/example/effectAndEdit/Addition.java
@@ -1,4 +1,13 @@
 package com.example.effectAndEdit;
 
-class Addition {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Addition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addition);
+    }
 }
