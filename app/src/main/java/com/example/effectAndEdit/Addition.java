23a4787a856b495b4c0bb084ebe5b912216be3fe
atieh <atiehbaratinia@gmail.com>
2019-06-25 15:32:15 +0430

    working on adding a sticker

--- app/src/main/java/com/example/effectAndEdit/Addition.java
+++ app/src/main/java/com/example/effectAndEdit/Addition.java
@@ -2,21 +2,27 @@ package com.example.effectAndEdit;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-public class Addition extends AppCompatActivity {
+public class Addition extends AppCompatActivity implements View.OnClickListener, View.OnDragListener {
     static Bitmap image;
     String type;
-    ImageView imageView;
+    ImageView imageView, sticker;
     EditText editText;
     ViewGroup viewGroup;
+    Button btn;
+    private int xDelta;
+    private int yDelta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +33,10 @@ public class Addition extends AppCompatActivity {
         image = FirstPage.Companion.getImageFile();
         imageView = findViewById(R.id.image_addition);
         imageView.setImageBitmap(image);
-        editText = findViewById(R.id.edittext);
+        //editText = findViewById(R.id.edittext);
         viewGroup = (ViewGroup)findViewById(R.id.root_layout);
+        btn = findViewById(R.id.button_addition_next);
+        btn.setOnClickListener(this);
         if (type.equals("text")){
             setText();
         }else if(type.equals("frame")){
@@ -40,21 +48,75 @@ public class Addition extends AppCompatActivity {
     }
 
     private void setSticker() {
+//        new AlertDialog.Builder(this).setItems({})
+        sticker = new ImageView(this);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150,150);
+        sticker.setImageResource(R.drawable.smiling_face_with_heart_shaped_eyes_1f60d);
+        sticker.setLayoutParams(layoutParams);
+        sticker.setOnTouchListener(new ChoiceTouchListener());
+        viewGroup.addView(sticker);
     }
 
     private void setFrame() {
     }
 
     private void setText() {
+        editText = new EditText(this);
+        editText.setHint("type here");
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150,150);
         editText.setLayoutParams(layoutParams);
-        editText.setOnTouchListener(new ChoiceTouchListener());
+        editText.setPadding(20,20,20,20);
+        editText.setFocusable(true);
+        viewGroup.addView(editText);
+        editText.setEnabled(true);
+        viewGroup.setOnTouchListener(new ChoiceTouchListener());
+        viewGroup.setOnDragListener(this);
+
+        //editText.setOnTouchListener(new ChoiceTouchListener());
+        //editText.setOnDragListener(this);
         //imageView.setOnTouchListener(new ChoiceTouchListener());
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_addition_next:
+
+        }
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        final int X = (int) event.getX();
+        final int Y = (int) event.getY();
+        switch (event.getAction() & MotionEvent.ACTION_MASK){
+            case MotionEvent.ACTION_DOWN:
+                RelativeLayout.LayoutParams lparams =(RelativeLayout.LayoutParams)v.getLayoutParams();
+                xDelta = X - lparams.leftMargin;
+                yDelta = Y - lparams.topMargin;
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)v.getLayoutParams();
+                layoutParams.leftMargin = X - xDelta;
+                layoutParams.topMargin = Y - yDelta;
+                layoutParams.rightMargin = -250;
+                layoutParams.bottomMargin = -250;
+                v.setLayoutParams(layoutParams);
+                break;
+        }
+        viewGroup.invalidate();
+        return true;
+    }
+
     private class ChoiceTouchListener implements View.OnTouchListener {
-        private int xDelta;
-        private int yDelta;
+
+
 
         @Override
         public boolean onTouch(View v, MotionEvent event) {
