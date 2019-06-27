a179557f3e638e34c4f9e6635f467d1c4d08c90c
mehrseno <m.seno@aut.ac.ir>
2019-06-27 18:55:08 +0430

    resize added with dialog box(bug)

--- app/src/main/java/com/example/effectAndEdit/SecondPage.java
+++ app/src/main/java/com/example/effectAndEdit/SecondPage.java
@@ -52,51 +52,50 @@ public class SecondPage extends AppCompatActivity implements View.OnClickListene
     @SuppressLint("ClickableViewAccessibility")
     public void resize() {
         final float[] percent = new float[1];
-        percent[0] = 25;
-        /*
+        int width = image.getWidth();
+        int height = image.getHeight();
+
         AlertDialog.Builder resizeDialog = new AlertDialog.Builder(this);
         resizeDialog.setTitle("Choose size");
-        String[] resizeDialogItems = {"200%", "175%", "150%", "125%", "110%", "90%", "75%", "50%", "25%"};
+        String[] resizeDialogItems = new String[8];
+        for (int i = 0; i < 8; i++) {
+            resizeDialogItems[i] = (i + 1) * 0.25 * width + " : " + (i + 1) * 0.25 * height;
+        }
         resizeDialog.setItems(resizeDialogItems, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 switch (which) {
-                    case 0:
+                    case 7:
                         percent[0] = 200;
                         break;
-                    case 1:
+                    case 6:
                         percent[0] = 175;
                         break;
-                    case 2:
+                    case 5:
                         percent[0] = 150;
                         break;
-                    case 3:
+                    case 4:
                         percent[0] = 125;
                         break;
-                    case 4:
-                        percent[0] = 110;
+                    case 3:
+                        percent[0] = 100;
                         break;
-                    case 5:
-                        percent[0] = 90;
-                        break;
-                    case 6:
+                    case 2:
                         percent[0] = 75;
                         break;
-                    case 7:
+                    case 1:
                         percent[0] = 50;
                         break;
-                    case 8:
+                    case 0:
                         percent[0] = 25;
                         break;
                 }
             }
         });
         resizeDialog.show();
-*/
+
 
         System.out.println("Resize Clicked.");
-        int width = image.getWidth();
-        int height = image.getHeight();
         float scaleWidth = percent[0] / 100;
         float scaleHeight = percent[0] / 100;
 
